90edbad5d7239ec148c99b4cb7ebc97a48912425
Maxime Castres <mcastres@student.42.fr>
2020-03-02 10:50:36 +0100

    Remove parseint function

--- frontend/src/components/Query/index.js
+++ frontend/src/components/Query/index.js
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/react-hooks";
 
 const Query = ({ children, query, id }) => {
   const { data, loading, error } = useQuery(query, {
-    variables: { id: parseInt(id) }
+    variables: { id: id }
   });
 
   if (loading) return <p>Loading...</p>;
